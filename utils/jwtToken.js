bfbbe828841999ce4fb68fa0591bacd12ac9af7e
jitzk07 <jittu1999@live.com>
2024-12-21 21:10:44 +0530

    production

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,14 +4,15 @@ export const sendToken = (user, statusCode, res, message) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Set httpOnly to true
+    httpOnly: true, // Prevent client-side access to cookies
+    secure: process.env.NODE_ENV === "production", // Send cookies only over HTTPS in production
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Allow cross-origin cookies in production
   };
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
     user,
     message,
-    token,
+    token, // Include token in response body for flexibility
   });
 };
